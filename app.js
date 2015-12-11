888ebd487bcdd49606508343fdb92164f0ce405f
Edmir Rocha <edmir@vstsistemas.com.br>
2015-12-10 22:51:19 -0200

    final aula

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ let express = require('express');
 let app = express();
 
 
+// Defini localizacao de arquivos estaticos
+app.use(express.static('public'));
+
+
 app.listen(3000, function(){
   console.log('> localhost:3000');
 });
